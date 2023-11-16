e60c6b06c5d5646350fbf79de3369182c1901986
TheHankMan <tom.hankins@outlook.com>
2023-11-16 17:42:11 +0000

    tabbed portfolio

--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -1,11 +1,20 @@
 import { Fragment } from "react";
+import styles from "../TeamStyles.module.css";
 
 const Gigs = () => {
     return (
         <Fragment>
-            <div className="container-fluid border">
-                <div>Gigs</div>
-                <div> SOME MORE</div>
+
+            <div class="container-fluid border">
+                <h1>Gigs</h1>
+                <div class="row">
+                    <div class="col-7">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                    <div class="col-5">
+                        <img src={require("../PFPs/Cat0.png")} alt="Liv" className={styles['pfpImgStyle']} />
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,42 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Bands from "./Bands";
 import Gigs from "./Gigs";
 import Venues from "./Venues";
 
+
+
+
 const Portfolio = () => {
+    const [curPage, setCurPage] = useState("Gigs");
+
+    const handleClick = (page) => {
+        console.log(curPage);
+        setCurPage(page);
+    }
     return (
         <Fragment>
-            <Gigs />
-            <Venues />
-            <Bands />
+            <div className="container" style={{padding: "40px"}}>
+                <div className="row">
+                    <div className="col-4 d-flex justify-content-center">
+                        <button onClick={() => handleClick("Gigs")} className="btn btn-primary" style={{minWidth: "150px"}}>
+                            <strong>Gigs</strong>
+                        </button>
+                    </div>
+                    <div className="col-4 d-flex justify-content-center">
+                        <button onClick={() => handleClick("Venues")} className="btn btn-primary" style={{ minWidth: "150px" }}>
+                            <strong>Venues</strong>
+                        </button>
+                    </div>
+                    <div className="col-4 d-flex justify-content-center">
+                        <button onClick={() => handleClick("Bands")} className="btn btn-primary" style={{ minWidth: "150px" }}>
+                            <strong>Clients/Bands</strong>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {curPage === "Gigs" && <Gigs />}
+            {curPage === "Venues" && <Venues />}
+            {curPage === "Bands" && <Bands />}
         </Fragment>
     )
 }
