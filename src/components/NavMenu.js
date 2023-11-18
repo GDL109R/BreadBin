64c62f810791d9c5103c849e90feb7f0de18c3e9
IamAwesomeGeorge <83273399+IamAwesomeGeorge@users.noreply.github.com>
2023-11-18 15:41:01 +0000

    Fixed logo front page link?
    This wasn't working so I added a NavLink. Not sure if it was just broken for me, so feel free to change it back if needed.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -13,7 +13,7 @@ function NavMenu() {
     return (
         <header>
             <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                <NavbarBrand to="/"><strong>BreadBin Productions</strong></NavbarBrand>
+                <NavbarBrand><NavLink tag={Link} to="/"><strong>BreadBin Productions</strong></NavLink></NavbarBrand>
                 <NavbarToggler onClick={toggleNavbar} className="mr-2" />
                 <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
                     <ul className="navbar-nav flex-grow">
